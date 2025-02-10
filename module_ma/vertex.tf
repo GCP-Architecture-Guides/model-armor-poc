0e04c93157f4a314084a8714bd3195b8dc842587
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2025-02-10 12:18:25 -0600

    Update vertex.tf

--- module_ma/vertex.tf
+++ module_ma/vertex.tf
@@ -1,4 +1,4 @@
-##  Copyright 2023 Google LLC
+##  Copyright 2025 Google LLC
 ##  
 ##  Licensed under the Apache License, Version 2.0 (the "License");
 ##  you may not use this file except in compliance with the License.
@@ -93,7 +93,7 @@ resource "google_workbench_instance" "ma_veretx_instance" {
 
   disable_proxy_access = "false"
 
-  instance_owners  = ["admin@manishkgaur.altostrat.com"]
+  instance_owners  = var.vertex_insance_owner
 
   labels = {
     k = "model-armor"
