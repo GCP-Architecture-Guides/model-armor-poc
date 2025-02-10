1c9b33d7b810f3fe28bc6d5ed7bbc535cf6d3e51
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2025-02-10 12:18:50 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-##  Copyright 2023 Google LLC
+##  Copyright 2025 Google LLC
 ##  
 ##  Licensed under the Apache License, Version 2.0 (the "License");
 ##  you may not use this file except in compliance with the License.
@@ -24,4 +24,5 @@ module "module_ma" {
   vpc_dmz_subnetwork = var.vpc_dmz_subnetwork
   roles              = var.roles
   ma_dlp_roles       = var.ma_dlp_roles
+  vertex_insance_owner = var.vertex_insance_owner
 }
