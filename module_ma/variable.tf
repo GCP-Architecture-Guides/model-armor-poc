085d1536a8dfe5fab70914bcbaa107c0941b460e
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2025-11-03 16:52:38 +0000

    Update variable.tf

--- module_ma/variable.tf
+++ module_ma/variable.tf
@@ -1,4 +1,4 @@
-##  Copyright 2025 Google LLC
+##  Copyright 2023 Google LLC
 ##  
 ##  Licensed under the Apache License, Version 2.0 (the "License");
 ##  you may not use this file except in compliance with the License.
@@ -23,13 +23,11 @@ variable "ma_project_id" {
 
 }
 
-
-variable "vertex_insance_owner" {
-   type        = list(string)
+variable "vertex_instance_owner" {
+  type        = list(string)
   description = "Vertex workbench instance owners"
 
 }
-
 
 variable "network_region" {
   type        = string
